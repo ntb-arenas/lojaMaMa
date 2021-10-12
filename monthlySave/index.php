8a1ec730b58d3795da1b9e3a1830dd198ede5926
vntarenas <ntb.arenas@protonmail.com>
2021-10-12 12:35:47 +0100

    added monthly savings proj

---
+++ monthlySave/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monthly Save</title>
+</head>
+<body>
+    <form action="result.php" method="POST">
+        <label for="moneyToSave">How much money you want to save a month?</label><br>
+        <input type="number" name="moneyToSave" id="moneyToSave"><br><br>
+        <label for="months">How many months?</label><br>
+        <input type="number" name="months" id="months"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
