1a0261659477c0ad4c4270bbaa9e80b225941dfb
vntarenas <ntb.arenas@protonmail.com>
2021-11-28 16:37:28 +0000

    .

--- main/userEntrar.php
+++ main/userEntrar.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
 session_start();
 
 // estabelecer conexão à base de dados
 
-include_once  './conexaobasedados.php'; 
+include_once  './conexaobasedados.php';
 
 
 $mensagemErroCodigo = "";
@@ -13,7 +13,7 @@ $codigo = "";
 $senha = "";
 
 
-if ( isset($_POST['botao-cancelar-entrada']) ) {
+if (isset($_POST['botao-cancelar-entrada'])) {
     header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
     header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // past date to encourage expiring immediately
     header("Location: index.php");
@@ -21,70 +21,64 @@ if ( isset($_POST['botao-cancelar-entrada']) ) {
 
 
 
-if ( isset($_POST['botao-iniciar-sessao']) ) {
-    
-    $codigo = strtolower(trim(mysqli_real_escape_string($_conn,$_POST["formCodigo"])));
+if (isset($_POST['botao-iniciar-sessao'])) {
+
+    $codigo = strtolower(trim(mysqli_real_escape_string($_conn, $_POST["formCodigo"])));
     $codigo = trim($codigo);
-    
-    $senha = trim(mysqli_real_escape_string($_conn,$_POST["formSenha"]));
+
+    $senha = trim(mysqli_real_escape_string($_conn, $_POST["formSenha"]));
     $senha = trim($senha);
-    
+
     $codigo = strip_tags($codigo);
-    
-    
+
+
     $stmt = $_conn->prepare('SELECT * FROM USERS WHERE CODIGO = ?');
-    $stmt->bind_param('s', $codigo); 
+    $stmt->bind_param('s', $codigo);
     $stmt->execute();
 
     $resultadoUsers = $stmt->get_result();
-    
+
     if ($resultadoUsers->num_rows > 0) {
         while ($rowUsers = $resultadoUsers->fetch_assoc()) {
-            
-            if ($rowUsers['USER_STATUS']==2) { // utilizador bloqueado
 
-                    $mensagemErroSenha="Não é possível entrar no sistema. Contacte os nossos serviços para obter ajuda.";
-                     
-                    } else  if ($rowUsers['USER_STATUS']==0 ) { // Utilizador criou a conta mas não ativou
+            if ($rowUsers['USER_STATUS'] == 2) { // utilizador bloqueado
 
-                                 $mensagemErroSenha=  $rowUsers['NOME'] . ", ainda não ativou a sua conta. A mensagem com o código inicial de ativação de conta foi enviada para a sua caixa de correio. Caso não a encontre na sua caixa de entrada, verifique também o seu correio não solicitado ou envie-nos um email para ativarmos a sua conta. Obrigado."; 
+                $mensagemErroSenha = "Não é possível entrar no sistema. Contacte os nossos serviços para obter ajuda.";
+            } else  if ($rowUsers['USER_STATUS'] == 0) { // Utilizador criou a conta mas não ativou
 
-                            } else  if ( password_verify($senha, $rowUsers["PASSWORD"])) {
-           
-            
-                $_SESSION["UTILIZADOR"]=$rowUsers["CODIGO"];
-                $_SESSION["NIVEL_UTILIZADOR"]=$rowUsers["NIVEL"];
-                $_SESSION["NOME_UTILIZADOR"]= $rowUsers["NOME"];
-                $_SESSION["EMAIL_UTILIZADOR"]= $rowUsers["EMAIL"];
-                
-              
+                $mensagemErroSenha =  $rowUsers['NOME'] . ", ainda não ativou a sua conta. A mensagem com o código inicial de ativação de conta foi enviada para a sua caixa de correio. Caso não a encontre na sua caixa de entrada, verifique também o seu correio não solicitado ou envie-nos um email para ativarmos a sua conta. Obrigado.";
+            } else  if (password_verify($senha, $rowUsers["PASSWORD"])) {
+
+
+                $_SESSION["UTILIZADOR"] = $rowUsers["CODIGO"];
+                $_SESSION["NIVEL_UTILIZADOR"] = $rowUsers["NIVEL"];
+                $_SESSION["NOME_UTILIZADOR"] = $rowUsers["NOME"];
+                $_SESSION["EMAIL_UTILIZADOR"] = $rowUsers["EMAIL"];
+
+
                 header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
                 header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // past date to encourage expiring immediately
                 header("Location: index.php");
             } else {
                 $mensagemErroSenha = "Senha incorreta!";
-                
-                
+
+
                 // encaminhar para página principal
                 header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
                 header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // past date to encourage expiring immediately
-                
+
                 header("Refresh: 3; URL=index.php"); // encaminhar 5 segundos depois
-                
-                
-                
+
+
+
             }
-   
-            
-            
         }
     } else {
         $mensagemErroCodigo = "O código de utilizador não existe na nossa base de dados!";
     }
-    
+
     $stmt->free_result();
     $stmt->close();
-    
 }
 
 ?>
@@ -92,33 +86,30 @@ if ( isset($_POST['botao-iniciar-sessao']) ) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Sistema X</title>
-<meta charset="UTF-8">
+    <title>Sistema X</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 
 <body>
 
+    <div class="main">
+        <p class="sign" align="center">Sign in</p>
+        <form class="form1" action="#" method="POST">
+            <input class="un " type="text" align="center" name="formCodigo" placeholder="Código de Utilizador" value="<?php echo $codigo; ?>" required>
 
+
+            <input class="pass" type="password" align="center" name="formSenha" placeholder="Senha" value="<?php echo $codigo; ?>" required>
+            <div class="div-submit">
+            <button name="botao-iniciar-sessao" type="submit" class="submit"> Iniciar Sessão</button>
             
-    <form action="#" method="POST">
-               
-        Código de utilizador:<?php echo $mensagemErroCodigo;?>
-        <br><input type="text" name="formCodigo" value="<?php echo $codigo;?>" required>
-        <br>
-        <br>Senha:<?php echo $mensagemErroSenha;?>
-        <br><input type="password" name="formSenha" value="<?php echo $senha;?>" required>
-        <br><br>
-        <button name="botao-iniciar-sessao" type="submit" > Iniciar Sessão</button>
-      </form>  
-    
-    <br>
-    
-    <form action="#" method="POST">
-          <button name="botao-cancelar-entrada" type="submit" > Cancelar</button>
-    </form>  
-  
-  
-   
+        </form>
+
+        <form action="#" method="POST">.
+        <button name="botao-cancelar-entrada" type="submit" class="submit"> Cancelar</button></div>
+        </form>
 </body>
+
 </html>
--- main/userRecuperarSenha.php
+++ main/userRecuperarSenha.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
 
 session_start();
 
-include_once  './conexaobasedados.php'; 
-include_once './function_mail_utf8.php'; 
+include_once  './conexaobasedados.php';
+include_once './function_mail_utf8.php';
 
 $mensagemErroCodigo = "";
 $mensagemErroSenha = "";
@@ -12,166 +12,159 @@ $mensagemEmail = "";
 $geraFormulario = "Sim";
 
 
-if ( isset($_POST['botao-recuperar-senha']) ) {
-    
+if (isset($_POST['botao-recuperar-senha'])) {
+
     $codigo = mysqli_real_escape_string($_conn, $_POST['formCodigo']);
     $codigo = strtolower(trim($codigo));
- 
-    
-        $stmt = $_conn->prepare('SELECT * FROM USERS WHERE CODIGO = ?');
-        $stmt->bind_param('s', $codigo); 
-        $stmt->execute();
 
-        $resultadoUsers = $stmt->get_result();
 
-        if ($resultadoUsers->num_rows > 0) {
-            while ($rowUsers = $resultadoUsers->fetch_assoc()) { 
-                
-                $nome = $rowUsers['NOME'];
+    $stmt = $_conn->prepare('SELECT * FROM USERS WHERE CODIGO = ?');
+    $stmt->bind_param('s', $codigo);
+    $stmt->execute();
 
-                if ($rowUsers['USER_STATUS']==2) { // utilizador bloqueado
+    $resultadoUsers = $stmt->get_result();
 
-                        $mensagemErroSenha="Não foi enviada mensagem de recuperação de senha, contacte os nossos serviços para obter ajuda.";
+    if ($resultadoUsers->num_rows > 0) {
+        while ($rowUsers = $resultadoUsers->fetch_assoc()) {
 
-                        } else  if ($rowUsers['USER_STATUS']==0 ) { // Utilizador criou a conta mas não ativou
+            $nome = $rowUsers['NOME'];
 
-                                     $mensagemErroSenha=  $rowUsers['NOME'] . ", ainda não ativou a sua conta. A mensagem com o código inicial de ativação de conta foi enviada para a sua caixa de correio. Caso não a encontre na sua caixa de entrada, verifique também o seu correio não solicitado ou envie-nos um email para ativarmos a sua conta. Obrigado."; 
+            if ($rowUsers['USER_STATUS'] == 2) { // utilizador bloqueado
 
-                                } else   {
+                $mensagemErroSenha = "Não foi enviada mensagem de recuperação de senha, contacte os nossos serviços para obter ajuda.";
+            } else  if ($rowUsers['USER_STATUS'] == 0) { // Utilizador criou a conta mas não ativou
+
+                $mensagemErroSenha =  $rowUsers['NOME'] . ", ainda não ativou a sua conta. A mensagem com o código inicial de ativação de conta foi enviada para a sua caixa de correio. Caso não a encontre na sua caixa de entrada, verifique também o seu correio não solicitado ou envie-nos um email para ativarmos a sua conta. Obrigado.";
+            } else {
 
                 // Recuperar a senha 
                 // gerar token, preparar e enviar mail de recuperação
-                
-                $code = md5(uniqid(rand()));
-                
 
-                $sql= "UPDATE  USERS SET TOKEN_CODE=? WHERE CODIGO=?";
-                
-                if ( $stmt = mysqli_prepare($_conn, $sql) ) {
-                                   
-                    mysqli_stmt_bind_param($stmt, "ss", $code,$codigo);
+                $code = md5(uniqid(rand()));
+
+
+                $sql = "UPDATE  USERS SET TOKEN_CODE=? WHERE CODIGO=?";
+
+                if ($stmt = mysqli_prepare($_conn, $sql)) {
+
+                    mysqli_stmt_bind_param($stmt, "ss", $code, $codigo);
                     mysqli_stmt_execute($stmt);
-                    
+
                     // /////////////////////////////////////////////////////////////////////////////////////////////////
                     // Update efetuado com sucesso, preparar e enviar mensagem /////////////////////////////////////////
                     $id = base64_encode($codigo);
-                    
+
                     $urlPagina = "http://localhost:8888/";
-                  
-                    
+
+
                     $mensagem = "Caro(a) $nome" . "," . "\r\n" .  "\r\n" .
 
-                            
-                        "Foi-nos pedido para recuperar a sua senha. Se nos pediu isto basta seguir as instruções seguintes, caso contrário, ignore esta mensagem.". "\r\n" .  "\r\n" .
 
-                        "Para recuperar agora a sua senha basta carregar na seguinte ligação:" ."\r\n" ."\r\n" .
+                        "Foi-nos pedido para recuperar a sua senha. Se nos pediu isto basta seguir as instruções seguintes, caso contrário, ignore esta mensagem." . "\r\n" .  "\r\n" .
 
-                        $urlPagina . "userNovaSenha.php?id=$id&code=$code" ."\r\n" ."\r\n" . 
+                        "Para recuperar agora a sua senha basta carregar na seguinte ligação:" . "\r\n" . "\r\n" .
 
-                        "Esta mensagem foi-lhe enviada automaticamente.";  
+                        $urlPagina . "userNovaSenha.php?id=$id&code=$code" . "\r\n" . "\r\n" .
+
+                        "Esta mensagem foi-lhe enviada automaticamente.";
 
                     $subject = "Recuperação da sua senha em $urlPagina";
 
                     // use wordwrap() if lines are longer than 70 characters
-                    $mensagem = wordwrap($mensagem,70);
+                    $mensagem = wordwrap($mensagem, 70);
 
                     // send email
-                    mail_utf8($email,$subject,$mensagem); 
+                    mail_utf8($email, $subject, $mensagem);
                     echo $mensagem; // apenas para efeitos de teste...
-                           
-                           
+
+
                     //$msgTemporaria = $email . " " . $subject . " " . $mensagem;
                     // mail enviado
-                   
-                    $mensagemEmail= " " . "$nome, verifique por favor a sua caixa de correio para recuperar de imediato a sua senha!";
-                   
-                    
+
+                    $mensagemEmail = " " . "$nome, verifique por favor a sua caixa de correio para recuperar de imediato a sua senha!";
+
+
                     //
                     // fim do envio de mensagem //////////////////////////////////////////////////////////////////            
-                        
+
                     $geraFormulario = "Nao";
-    
-                                    
                 } else {
                     // erro
                     echo "STATUS ADMIN (recuperar senha): " . mysqli_error($_conn);
-                }                  
-                                    
-                    
-                } 
-
-
-
+                }
             }
-        } else {
-            $mensagemErroCodigo = "O código de utilizador não existe na nossa base de dados!";
         }
-    
+    } else {
+        $mensagemErroCodigo = "O código de utilizador não existe na nossa base de dados!";
+    }
+
     $stmt->free_result();
     $stmt->close();
-    
 }
 
 
 
- 
+
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-<title>Recuperar senha</title>
-<meta charset="UTF-8">
-</head> 
+    <title>Recuperar senha</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/login.css">
+</head>
 
 <body>
 
-<?php 
+    <?php
 
-   if ( $geraFormulario == "Sim" ) { 
-   
-?>
-           
-<form action="#" method="POST">
-               
-          
-                <p>Introduza o código que utiliza para inciar sessão e carregue no botão para recuperar senha. Será enviada para o seu e-mail uma mensagem com um código de recuperação de senha.
-                <br> Por vezes estas mensagens são consideradas correio não solicitado. Se não vir a mensagem de recuperação verifique o seu correio não solicitado (SPAM).</p>
-                <br><p> </p>
-                <p>Código de utilizador para recuperação de senha:</p> 
-                <input type="text" name="formCodigo" id="formCodigo" value="<?php echo $codigo;?>" required>
-                <br><?php echo $mensagemEmail;?>
-                <br><?php echo $mensagemErroCodigo;?>
-                <br><?php echo $mensagemErroSenha;?>
-                <br>
-                <button name="botao-recuperar-senha" type="submit" > Recuperar senha agora</button>
-</form>  
+    if ($geraFormulario == "Sim") {
 
-<?php 
-   } else { 
-?>
-   <h2>Recuperação de senha</h2>
-   <p>
-   <br>Link de recuperação enviado. Verifique a sua caixa de correio.
-   
-   </p>
-   
-   <?php 
-   
-   // encaminhar para página principal
-   header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
-   header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // past date to encourage expiring immediately
-   
-   // Comentado para efeitos de teste (copy/paste do link de recuperação)
-   // header("Refresh: 5; URL=index.php"); // encaminhar 5 segundos depois
-           
-   ?>
+    ?>
 
-<?php
-   }   		
-?>
+        <form action="#" method="POST">
 
 
-            
+            <p>Introduza o código que utiliza para inciar sessão e carregue no botão para recuperar senha. Será enviada para o seu e-mail uma mensagem com um código de recuperação de senha.
+                <br> Por vezes estas mensagens são consideradas correio não solicitado. Se não vir a mensagem de recuperação verifique o seu correio não solicitado (SPAM).
+            </p>
+            <br>
+            <p> </p>
+            <p>Código de utilizador para recuperação de senha:</p>
+            <input type="text" name="formCodigo" id="formCodigo" value="<?php echo $codigo; ?>" required>
+            <br><?php echo $mensagemEmail; ?>
+            <br><?php echo $mensagemErroCodigo; ?>
+            <br><?php echo $mensagemErroSenha; ?>
+            <button name="botao-recuperar-senha" type="submit" class="submit"> Recuperar senha agora</button>
+        </form>
+
+    <?php
+    } else {
+    ?>
+        <h2>Recuperação de senha</h2>
+        <p>
+            <br>Link de recuperação enviado. Verifique a sua caixa de correio.
+
+        </p>
+
+        <?php
+
+        // encaminhar para página principal
+        header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // past date to encourage expiring immediately
+
+        // Comentado para efeitos de teste (copy/paste do link de recuperação)
+        // header("Refresh: 5; URL=index.php"); // encaminhar 5 segundos depois
+
+        ?>
+
+    <?php
+    }
+    ?>
+
+
+
 </body>
+
 </html>
