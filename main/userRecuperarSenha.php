cfd34c92db56da331c2e141f596ea3c2371f09e8
vntarenas <ntb.arenas@protonmail.com>
2021-12-01 14:34:57 +0000

    .

--- main/userRecuperarSenha.php
+++ main/userRecuperarSenha.php
@@ -64,11 +64,7 @@ if (isset($_POST['botao-recuperar-senha'])) {
 
                         "Para recuperar agora a sua senha basta carregar na seguinte ligação:" . "\r\n" . "\r\n" .
 
-<<<<<<< HEAD
-                        $urlPagina . "userNovaSenha.php?id=$id&code=$code" . "\r\n" . "\r\n" .
-=======
                         $urlPagina . "sistemaX/userNovaSenha.php?id=$id&code=$code" . "\r\n" . "\r\n" .
->>>>>>> 453e6db28d4fa390e587a13ff95f513136545cc2
 
                         "Esta mensagem foi-lhe enviada automaticamente.";
 
