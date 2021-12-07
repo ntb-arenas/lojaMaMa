e20649c468f018a373d16f4123933436d68d903d
vntarenas <ntbarenas@gmail.com>
2021-12-07 10:46:51 +0000

    .

--- PROJETOALMOFADAS/loginSession/function_mail_utf8.php
+++
@@ -1,6 +0,0 @@
-<?php 
-function mail_utf8($to, $subject = '(No subject)', $message = '', $header = '') {
-  $header_ = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/plain; charset=UTF-8' . "\r\n";
-  mail($to, '=?UTF-8?B?'.base64_encode($subject).'?=', $message, $header_ . $header);
-}
-?>
