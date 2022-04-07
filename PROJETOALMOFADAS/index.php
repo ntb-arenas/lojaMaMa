1f91d11ebc3b0943168f8ca7dc2a52fb38e9f191
vntarenas <ntb.arenas@protonmail.com>
2022-04-07 11:52:39 +0100

    .

--- PROJETOALMOFADAS/index.php
+++ PROJETOALMOFADAS/index.php
@@ -1,29 +1,6 @@
 <?php
 session_start();
 include_once  './loginSession/connect_DB.php';
-
-// $dbname = 'id17632851_pap';
-// $dbuser = 'id17632851_projetoalmo';
-// $dbpass = 'Bp[84zQW+)WU5Cs6';
-// $dbhost = 'localhost';
-
-// $connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-// mysqli_select_db($dbname) or die("Could not open the db '$dbname'");
-
-// $test_query = "SHOW TABLES FROM $dbname";
-// $result = mysqli_query($test_query);
-
-// $tblCnt = 0;
-// while($tbl = mysqli_fetch_array($result)) {
-//   $tblCnt++;
-//   #echo $tbl[0]."<br />\n";
-// }
-
-// if (!$tblCnt) {
-//   echo "There are no tables<br />\n";
-// } else {
-//   echo "There are $tblCnt tables<br />\n";
-// }
 ?>
 
 <!DOCTYPE html>
@@ -34,7 +11,8 @@ include_once  './loginSession/connect_DB.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ma-Ma</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.scss">
     <link rel="shortcut icon" href="gallery/logo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -44,76 +22,96 @@ include_once  './loginSession/connect_DB.php';
 
 <body>
     <main>
-        <!--Header starts here-->
-        <header>
-            <div class="logo">
-                <a href="./index.php">
-                    <img src="gallery/logo.png" alt="Ma-ma logo" class="logo">
-                </a>
-            </div>
-
-            <div class="search-bar">
-                <input type="search" placeholder="Encontre o produto de que precisa...">
-                <span><img src="gallery/searchBtn.png" id="searchBtn"></span>
-            </div>
-
-            <?php
-            if (isset($_SESSION["USER"])) { ?>
-                <div class="divIcon">
-                    <span><a href="like.html"><img src="gallery/like.png" id="likeBtn"></a></span>
-                    <span><a href="./profileAccount.php"><img src="gallery/user.png" id="userBtn"></a></span>
-                    <span><a href="cart.html"><img src="gallery/cart.png" id="cartBtn"></a></span>
+        <div class="container-fluid p-0">
+            <div class="row">
+                <div class="col-6 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                    <a href="./index.php">
+                        <img class="img-fluid" src="gallery/logo.png" alt="Ma-ma logo" class="logo">
+                    </a>
                 </div>
-            <?php } else { ?>
-                <div class="divIcon">
-                    <span><a href="like.html"><img src="gallery/like.png" id="likeBtn"></a></span>
-                    <span><a href="./loginSession/login.php"><img src="gallery/user.png" id="userBtn"></a></span>
-                    <span><a href="cart.html"><img src="gallery/cart.png" id="cartBtn"></a></span>
+                <form class="col-sm-6 col-md-7 col-lg-6 col-xl-7 mt-3 custom-class">
+                    <div class="row justify-content-center">
+                        <div class="col-sm-9">
+                            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                        </div>
+                        <div class="col-sm-3">
+                            <button class="btn btn-outline-warning p-sm-6 " style="color: #f35518;" type="submit">Search</button>
+                        </div>
+                    </div>
+                </form>
+                <div class="col-6 col-sm-3 col-md-2 col-lg-3 col-xl-2 mt-3"><?php
+                                                                            if (isset($_SESSION["USER"])) { ?>
+                        <a href="like.html"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+                                <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
+                            </svg></a>
+                        <a href="./profileAccount.php"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+                                <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
+                                <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z" />
+                            </svg></a>
+                        <a href="cart.html"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                            </svg></a>
+
+                    <?php } else { ?>
+                        <div class="d-flex justify-content-evenly col-12">
+                            <a href="like.html" style="color: #f35518;"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+                                    <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
+                                </svg></a>
+                            <a href="./loginSession/login.php" style="color: #f35518;"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+                                    <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
+                                    <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z" />
+                                </svg></a>
+                            <a href="cart.html" style="color: #f35518;"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                                </svg></a>
+                        </div>
+                    <?php } ?>
                 </div>
-            <?php } ?>
-        </header>
-        <!--Header ends here-->
+            </div>
+        </div>
 
         <!--Navbar starts here-->
-        <div class="navBar">
-            <input type="checkbox" id="click">
-            <label for="click" class="menu-btn">
-                <i class="fas fa-bars"></i>
-            </label>
-            <ul>
-                <?php
-                $resultTablecategory = mysqli_query($_conn, "SELECT * FROM CATEGORY WHERE VISIBLE = 1 ORDER BY SEQUENCE ASC");
-                if (mysqli_num_rows($resultTablecategory) > 0) {
-                    $ctd = 0;
-                    while ($rowTablecategory = mysqli_fetch_assoc($resultTablecategory)) {
-                        $ctd = $ctd + 1;
-                ?>
-
-                        <li><a href="<?php echo $rowTablecategory['LINK'] ?>"><?php echo $rowTablecategory['TITLE'] ?></a></li>
-                <?php
-                    }
-                }
-                mysqli_free_result($resultTablecategory);
-                ?>
-            </ul>
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <ul class="navbar-nav justify-content-around col-12">
+                        <?php
+                        $resultTablecategory = mysqli_query($_conn, "SELECT * FROM CATEGORY WHERE VISIBLE = 1 ORDER BY SEQUENCE ASC");
+                        if (mysqli_num_rows($resultTablecategory) > 0) {
+                            $ctd = 0;
+                            while ($rowTablecategory = mysqli_fetch_assoc($resultTablecategory)) {
+                                $ctd = $ctd + 1;
+                        ?>
+                                <li class="nav-item"><a href="<?php echo $rowTablecategory['LINK'] ?>" class="nav-link fs-5"> <?php echo $rowTablecategory['TITLE'] ?></a></li>
+                        <?php
+                            }
+                        }
+                        mysqli_free_result($resultTablecategory);
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <!--Navbar ends here-->
 
-        <!-- Page cover starts here-->
-        <div class="coverDiv">
-            <div class="titleDiv">
-                <h1>Conforto e Bem-estar <br> ao Melhor Preço</h1>
-            </div>
-            <div class="divImage">
-                <img src="gallery/almofadaAmam.webp" id="coverImg">
-            </div>
+        <!-- Page cover -->
+        <img class="img-fluid mx-auto d-block custom-main-size" src="./gallery/maincover.png" alt="">
+        <img class="img-fluid mx-auto d-block custom-tablet-size" src="./gallery/maincovertabletsize.png" alt="">
+        <img class="img-fluid mx-auto d-block custom-mobile-size" src="./gallery/maincovermobilesize.png" alt="">
+        <!-- Page cover -->
+        <div class="container-fluid">
+            <p class="h3">Com a Ma-Ma, a vida da mãe e do seu bebé nunca foi tão fácil. Descubra os nossos produtos!</p>
         </div>
-        <div class="titleMsg">
-            <p style="margin: 0 10px 0 10px;">Com a Ma-Ma, a vida da mãe e do seu bebé nunca foi tão fácil. Descubra os nossos produtos!</p>
-        </div>
+
+
         <!--Page cover ends here-->
 
-        <!--Features section starts here-->
+
+
+        <!--Features section starts here
         <section class="features" id="features">
             <div class="box-container">
                 <div class="box">
@@ -172,9 +170,9 @@ include_once  './loginSession/connect_DB.php';
                 </div>
             </div>
         </section>
-        <!--Features section ends here-->
+        Features section ends here-->
 
-        <footer>
+        <!-- <footer>
             <div class="coverFooter">
                 <div class="logoContainer">
                     <div class="logo">
@@ -222,8 +220,9 @@ include_once  './loginSession/connect_DB.php';
                     </div>
                 </div>
             </div>
-        </footer>
+        </footer> -->
     </main>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </html>
