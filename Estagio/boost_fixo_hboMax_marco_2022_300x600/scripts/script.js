6665a8f7c2e5ccc1e08f388ac627ce0fdd587d3e
ntb-arenas <91438229+ntb-arenas@users.noreply.github.com>
2022-04-13 22:37:01 +0100

    vodafone test

---
+++ Estagio/boost_fixo_hboMax_marco_2022_300x600/scripts/script.js
@@ -0,0 +1,19 @@
+﻿var load = function () {
+  setTimeout(function () {
+    $(".copy2").addClass("op-1 anim-top");
+    setTimeout(function () {
+      $(".copy3").addClass("op-1 anim-top");
+      setTimeout(function () {
+        $(".call").addClass("op-1 full");
+        setTimeout(function () {
+          $(".call").removeClass("full");
+          setTimeout(function () {
+            $(".call").addClass("full");
+          }, 500);
+        }, 500);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+};
+
+setTimeout(load, 0);
